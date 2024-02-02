0366c9837801abc1c4910be3ca580ca6f2f911c2
igillespie156 <ian.t.gillespie@gmail.com>
2024-02-02 10:05:43 -0500

    Added the add button

--- To-Do App/gui.py
+++ To-Do App/gui.py
@@ -1,15 +1,34 @@
-import Functions
+import Functions as fun
 
 import PySimpleGUI as pg
 
 label = pg.Text("Type in a to-do:")
-input_box = pg.InputText(tooltip="Enter to-do")
+input_box = pg.InputText(tooltip="Enter to-do", key = "todo")
 add_button = pg.Button("Add")
 
-window = pg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = pg.Window('My To-Do App',
+                   layout=[[label],
+                           [input_box, add_button]],
+                   font=('Helvetica', 20))
 
-window.read()
-print("hello")
+
+while True:
+    event, values = window.read()
+
+    match event:
+        case "Add":
+            todos = fun.get_todos()
+            new_todo = values["todo"] +'\n'
+            todos.append(new_todo)
+            fun.write_todos(new_todo)
+        case pg.WIN_CLOSED:
+            break
+
+
+
+    print(todos)
+
+    print(event)
 window.close()
 
 
