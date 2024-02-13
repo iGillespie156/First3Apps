f358946a759f45b77b7ebbc3247a63080d62cacd
igillespie156 <ian.t.gillespie@gmail.com>
2024-02-13 16:47:01 -0500

    Adding new items to the todo list webapp

--- To-Do App/web.py
+++ To-Do App/web.py
@@ -3,15 +3,21 @@ import Functions as fun
 
 todos = fun.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    fun.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is designed to increase your productivity")
 
-st.checkbox("Buy grocery")
-st.checkbox("Throw the trash")
 
 
 for i in todos:
     st.checkbox(i)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+st.session_state
