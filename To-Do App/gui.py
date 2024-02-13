9bc32691d02ec5b3d8e44dad6519efc7fe947ec1
igillespie156 <ian.t.gillespie@gmail.com>
2024-02-13 13:09:51 -0500

    Completed all buttons for the gui

--- To-Do App/gui.py
+++ To-Do App/gui.py
@@ -5,6 +5,8 @@ import PySimpleGUI as pg
 label = pg.Text("Type in a to-do:")
 input_box = pg.InputText(tooltip="Enter to-do", key="todo")
 add_button = pg.Button("Add")
+complete_button = pg.Button("Complete")
+close_button = pg.Button("Exit")
 list_box = pg.Listbox(values=fun.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = pg.Button("Edit")
@@ -12,7 +14,8 @@ edit_button = pg.Button("Edit")
 window = pg.Window('My To-Do App',
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, complete_button],
+                           [close_button]],
                    font=('Helvetica', 20))
 
 
@@ -40,6 +43,18 @@ while True:
 
         case "todos":
             window["todo"].update(value=values['todos'][0])
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = fun.get_todos()
+            todo_index =todos.index(todo_to_complete)
+            todos.pop(todo_index)
+            fun.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(values='')
+
+        case "Exit":
+            break
 
         case pg.WIN_CLOSED:
             break
